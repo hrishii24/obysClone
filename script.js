78c5a549b5932b5499b38054dfaea36410a66877
Hrishikesh Kolhe <153733707+hrishii24@users.noreply.github.com>
2024-01-30 17:37:15 +0530

    page3 almost done

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function loco() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+
 function loadingAnimation() {
   var tl = gsap.timeline();
 
@@ -62,24 +97,55 @@ function loadingAnimation() {
   });
 }
 
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#cursor", {
+      left: dets.x,
+      top: dets.y,
+      duration: 0.3,
+    });
+  });
+}
+
+function sheryAnimation() {
+  Shery.imageEffect(".image-div", {
+    style: 5,
+    config: {
+      a: { value: 0.92, range: [0, 30] },
+      b: { value: 0.75, range: [-1, 1] },
+      zindex: { value: -9996999, range: [-9999999, 9999999] },
+      aspect: { value: 0.7272695760684946 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: false },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: true },
+      maskVal: { value: 1.34, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 1 },
+      noise_speed: { value: 0.61, range: [0, 10] },
+      metaball: { value: 0.99, range: [0, 2] },
+      discard_threshold: { value: 0.88, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.31, range: [0, 2] },
+      noise_scale: { value: 7.63, range: [0, 100] },
+    },
+    gooey: true,
+  });
+}
+
 loadingAnimation();
-
-document.addEventListener("mousemove", function (dets) {
-  gsap.to("#cursor", {
-    left: dets.x,
-    top: dets.y,
-    duration: 0.3,
-  });
-});
-
-let link = document.querySelectorAll(".link").forEach((link) => {
-  link.addEventListener("mousemove", (e) => {
-    let x = e.offsetX;
-    let y = e.offsetY;
-    let linkWidth = link.clientWidth;
-    let linkHeight = link.clientHeight;
-    let transX = x - linkWidth / 2;
-    let transY = y - linkHeight / 2;
-    link.style.transform = `translateX(${transX}px)translateY(${transY}px)`;
-  });
-});
+cursorAnimation();
+// loco();
+sheryAnimation();
